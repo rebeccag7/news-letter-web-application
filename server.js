b027a19a64dc790ebff1a51970bebffd4429dd58
Rebecca Goveia <rebeccagoveia@gmail.com>
2017-06-27 16:18:19 -0700

    Added templating engine

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const request = require('request');
 const async = require('async');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(morgan('dev'));
 
 app.get('/', (req, res, next) => {
-	res.json("Hello world");
+	res.render('main/home');
 });
 
 app.listen(3000, (err) => {
